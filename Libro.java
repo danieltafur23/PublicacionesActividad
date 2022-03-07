62fbd944ee2dad9616b5e14d6c5c5dc53ef20cae
danieltafur23 <79168401+danieltafur23@users.noreply.github.com>
2022-03-07 17:30:27 -0500

    Create Libro.java

---
+++ Libro.java
@@ -0,0 +1,27 @@
+package ActividadClase1;
+
+
+public class Libro extends Publicacion {
+    
+ private boolean prestado;
+ 
+ 
+    public boolean isPrestado() {
+        return prestado;
+    }
+
+    public void setPrestado(boolean prestado) {
+        this.prestado = prestado;
+    }
+    
+    public Libro(String codigo, String titulo, int anoPublicacion) {
+        super(codigo, titulo, anoPublicacion);
+        this.prestado = false;
+    }
+    
+    public void prestar(){
+        prestado = true;
+    }
+    
+    
+}
